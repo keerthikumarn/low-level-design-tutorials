777c75c08b06c75102f64fc6843dc72b89468c1e
keerthikumarn <n.keerthikumar85@gmail.com>
2024-05-06 11:07:24 +0530

    created the model records of crew and passenger

---
+++ src/main/java/com/lld/flightbooking/Crew.java
@@ -0,0 +1,5 @@
+package com.lld.flightbooking;
+
+public record Crew(String name, int employeeId) {
+
+}
---
+++ src/main/java/com/lld/flightbooking/Passenger.java
@@ -0,0 +1,5 @@
+package com.lld.flightbooking;
+
+public record Passenger(String name, String passportNum) {
+
+}
