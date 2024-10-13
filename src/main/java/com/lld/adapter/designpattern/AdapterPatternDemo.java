3749932de0cee863580d7b219b513d6479988eae
Keerthi Kumar <n.keerthikumar85@gmail.com>
2024-10-13 13:10:19 +0530

    added the client class

--- src/main/java/com/lld/adapter/designpattern/AdapterPatternDemo.java
+++ src/main/java/com/lld/adapter/designpattern/AdapterPatternDemo.java
@@ -3,8 +3,12 @@ package com.lld.adapter.designpattern;
 public class AdapterPatternDemo {
 
 	public static void main(String[] args) {
-
+		AdvancedMediaPlayer player = new AdvancedMediaPlayer();
+		player.play("mp3", "test_song_1.mp3");
+		player.play("wav", "test_song_2.wav");
+		player.play("mkv", "movie1.mkv");
+		player.play("mp4", "new_movie.mp4");
+		player.play("mp5", "latest_movie.mp5");
 	}
 
 }
-	
